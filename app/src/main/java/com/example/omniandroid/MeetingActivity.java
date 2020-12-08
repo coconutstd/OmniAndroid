b7c28674f1bc2c2a1ffd6c877e991ae4be16ff16
junui <wnsspdl@naver.com>
2020-12-08 13:03:42 +0900

    웹뷰 해결

--- app/src/main/java/com/example/omniandroid/MeetingActivity.java
+++ app/src/main/java/com/example/omniandroid/MeetingActivity.java
@@ -24,7 +24,16 @@ public class MeetingActivity extends AppCompatActivity {
         mWebView = (WebView)this.findViewById(R.id.webview_meeting);
         mWebView.setWebChromeClient(new WebChromeClient());
         mWebSettings = mWebView.getSettings();
-        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setJavaScriptEnabled(true); // 웹페이지 자바스클비트 허용 여부
+        mWebSettings.setSupportMultipleWindows(false); // 새창 띄우기 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); // 자바스크립트 새창 띄우기(멀티뷰) 허용 여부
+        mWebSettings.setLoadWithOverviewMode(true); // 메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true); // 화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false); // 화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false); // 화면 확대 축소 허용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN); // 컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE); // 브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true); // 로컬저장소 허용 여부
         mWebView.loadUrl("https://main.d2va81juaem8l3.amplifyapp.com/videochat");
 
 //      앱 캐시 활성화 코드(비추천...)
