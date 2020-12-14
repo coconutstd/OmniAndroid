8d8e789024b42b181adbc835d317be8745112fa4
junui <wnsspdl@naver.com>
2020-12-14 16:51:53 +0900

    Amplify 안드로이드와, 뷰앱 통합  (소셜로그인 제거)

---
+++ app/src/main/java/sensorApi/SensorApiClient.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package sensorApi;
+
+import java.util.*;
+
+
+
+@com.amazonaws.mobileconnectors.apigateway.annotation.Service(endpoint = "https://5zz1b56tcf.execute-api.ap-northeast-2.amazonaws.com/dev")
+public interface SensorApiClient {
+
+
+    /**
+     * A generic invoker to invoke any API Gateway endpoint.
+     * @param request
+     * @return ApiResponse
+     */
+    com.amazonaws.mobileconnectors.apigateway.ApiResponse execute(com.amazonaws.mobileconnectors.apigateway.ApiRequest request);
+    
+    /**
+     * 
+     * 
+     * @return void
+     */
+    @com.amazonaws.mobileconnectors.apigateway.annotation.Operation(path = "/sensor", method = "OPTIONS")
+    void sensorOptions();
+    
+    /**
+     * 
+     * 
+     * @return void
+     */
+    @com.amazonaws.mobileconnectors.apigateway.annotation.Operation(path = "/sensor/{proxy+}", method = "OPTIONS")
+    void sensorProxyOptions();
+    
+}
+
