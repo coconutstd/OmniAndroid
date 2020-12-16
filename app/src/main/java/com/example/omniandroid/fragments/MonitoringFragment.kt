d071e993c18ae08d8020294f033800bb3cd40afc
hyeju <kwonhyeju0531@gmail.com>
2020-12-16 13:33:13 +0900

    No adapter attached

--- app/src/main/java/com/example/omniandroid/fragments/MonitoringFragment.kt
+++ app/src/main/java/com/example/omniandroid/fragments/MonitoringFragment.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.omniandroid.R
 import com.example.omniandroid.SensorItem
 import com.example.omniandroid.SensorService
 import kotlinx.android.synthetic.main.fragment_monitoring.*
+import kotlinx.android.synthetic.main.fragment_monitoring.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,18 +22,28 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MonitoringFragment : Fragment() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val adapter = CustomAdapter()
 
-        // 이 부분만 좀 다름!
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val adapter = CustomAdapter();
+        val view = inflater.inflate(R.layout.fragment_monitoring, container, false) as ViewGroup
+        val recyclerView = view.findViewById<View>(R.id.recyclerView) as RecyclerView
+        val buttonRequest = view.findViewById<View>(R.id.buttonRequest) as Button
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(getContext())
 
         val retrofit = Retrofit.Builder()
-                .baseUrl("https://4cxysyupk7.execute-api.ap-northeast-2.amazonaws.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
+            .baseUrl("https://4cxysyupk7.execute-api.ap-northeast-2.amazonaws.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
         buttonRequest.setOnClickListener {
             val sensorService = retrofit.create(SensorService::class.java)
@@ -44,12 +57,6 @@ class MonitoringFragment : Fragment() {
                 }
             })
         }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_monitoring, container, false)
     }
@@ -59,4 +66,6 @@ class MonitoringFragment : Fragment() {
             return MonitoringFragment()
         }
     }
+
+
 }
